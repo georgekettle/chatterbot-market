cb87a79c8912974654fd94f2dd8c97cdd72a3c3c
george kettle <georgekettle.gk@gmail.com>
2023-03-24 13:58:36 +1100

    Added devise authentication

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './node_modules/flowbite/**/*.js'
+  ],
+
+  theme: {
+    fontFamily: {
+      sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+    },
+    colors: {
+      primary: colors.orange,
+      secondary: colors.sky,
+      tertiary: colors.slate,
+      success: colors.green,
+      danger: colors.red,
+    }
+  },
+
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+    require('flowbite/plugin'),
   ]
 }
