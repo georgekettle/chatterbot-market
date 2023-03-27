82ed141275b03791a0e7481abf6c0964c174469a
george kettle <georgekettle.gk@gmail.com>
2023-03-27 11:54:54 +1100

    Basic typography and buttons added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,35 @@ module.exports = {
 
   theme: {
     fontFamily: {
-      sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+      sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
     },
     colors: {
-      primary: colors.amber,
-      secondary: colors.purple,
-      tertiary: colors.stone,
+      // primary: {
+      //     '50': '#f1f2fe',
+      //     '100': '#e1e6fe',
+      //     '200': '#cad0fc',
+      //     '300': '#a9b0f9',
+      //     '400': '#8688f4',
+      //     '500': '#766eed',
+      //     '600': '#604ce1',
+      //     '700': '#533fc6',
+      //     '800': '#41339e',
+      //     '900': '#38317d',
+      // },
+      primary: {
+          '50': '#fff9eb',
+          '100': '#ffefc6',
+          '200': '#ffde88',
+          '300': '#ffcf66',
+          '400': '#ffaf20',
+          '500': '#f98c07',
+          '600': '#dd6602',
+          '700': '#b74506',
+          '800': '#94340c',
+          '900': '#7a2c0d',
+      },
+      secondary: colors.amber,
+      tertiary: colors.neutral,
       success: colors.green,
       danger: colors.red,
     }
