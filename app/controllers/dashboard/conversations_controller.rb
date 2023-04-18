2294e6cf77f3029c1fcb226607c113e326070b49
george kettle <georgekettle.gk@gmail.com>
2023-04-18 14:12:20 -0400

    Conversations working on dashboard

--- app/controllers/dashboard/conversations_controller.rb
+++ app/controllers/dashboard/conversations_controller.rb
@@ -1,12 +1,15 @@
 class Dashboard::ConversationsController < ApplicationController
+  layout 'dashboard_chatbot', only: :index
+
   # GET /dashboard/chatbots/:chatbot_id/conversations
   def index
     @chatbot = Chatbot.find(params[:chatbot_id])
     @pagy, @conversations = pagy(
       policy_scope(@chatbot.conversations).order(created_at: :desc)
     )
+    @selected_conversation = params[:conversation_id].present? ? Conversation.find(params[:conversation_id]) : @conversations.first
 
-    breadcrumb "Dashboard", dashboard_chatbots_path
+    breadcrumb "My Chatbots", dashboard_chatbots_path
     breadcrumb @chatbot.name.capitalize, request.path
   end
 end
