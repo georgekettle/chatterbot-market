8a7566adcd98172661226ca690cee81ee6f0c205
george kettle <georgekettle.gk@gmail.com>
2023-04-21 15:48:58 -0400

    Added read_at to feedback

---
+++ db/migrate/20230421194728_add_read_at_to_feedback.rb
@@ -0,0 +1,5 @@
+class AddReadAtToFeedback < ActiveRecord::Migration[7.0]
+  def change
+    add_column :feedbacks, :read_at, :datetime, default: nil
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_040428) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_194728) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_040428) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "read_at"
     t.index ["message_id"], name: "index_feedbacks_on_message_id"
     t.index ["user_id"], name: "index_feedbacks_on_user_id"
   end
