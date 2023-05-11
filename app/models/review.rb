3512ec329aea6660915eeae8ab51d33529259fc5
george kettle <georgekettle.gk@gmail.com>
2023-05-11 19:53:44 -0400

    No update after 1 day

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,11 @@ class Review < ApplicationRecord
   
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :comment, presence: true, length: { maximum: 500 }
+  validate :no_update_after_5_days, on: :update
+
+  def no_update_after_5_days
+    if created_at && created_at < 1.days.ago
+      errors.add(:base, "Cannot edit review after 5 days of creation.")
+    end
+  end
 end
